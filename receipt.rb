003e30fd1b17fdd22267f8fff9a377b5e80250fb
Brandon Toone <780144+btoone@users.noreply.github.com>
2025-01-14 20:03:37 -0700

    Whitespace

--- receipt.rb
+++ receipt.rb
@@ -1,27 +1,27 @@
 module ShoppingBasket
-class Receipt
-  # attr_reader :order
+  class Receipt
+    # attr_reader :order
 
-  def initialize(order)
-    @order = order
-  end
+    def initialize(order)
+      @order = order
+    end
 
-  def to_s
-    [items, total_tax, total_cost].join
-  end
+    def to_s
+      [items, total_tax, total_cost].join
+    end
 
-  def items
-    @order.items.map do |item|
-      "#{item[:quantity]} #{item[:title]}: #{'%.2f' % item[:subtotal]}\n"
-    end.join
-  end
+    def items
+      @order.items.map do |item|
+        "#{item[:quantity]} #{item[:title]}: #{'%.2f' % item[:subtotal]}\n"
+      end.join
+    end
 
-  def total_tax
-    "Sales Taxes: #{'%.2f' % @order.total_tax}\n"
-  end
+    def total_tax
+      "Sales Taxes: #{'%.2f' % @order.total_tax}\n"
+    end
 
-  def total_cost
-    "Total: #{'%.2f' % @order.total_cost}\n"
+    def total_cost
+      "Total: #{'%.2f' % @order.total_cost}\n"
+    end
   end
-end
 end
--- receipt_test.rb
+++ receipt_test.rb
@@ -1,70 +1,70 @@
 require_relative './test_helper'
 
 module ShoppingBasket
-class ReceiptTest < Minitest::Test
-  Order = Struct.new(:items, :total_tax, :total_cost)
+  class ReceiptTest < Minitest::Test
+    Order = Struct.new(:items, :total_tax, :total_cost)
 
-  def setup
-    @items = [
-      {quantity: 2, title: "book", subtotal: 24.98},
-      {quantity: 1, title: "music CD", subtotal: 16.49},
-      {quantity: 1, title: "chocolate bar", subtotal: 0.85},
-    ]
-    @order = Order.new(@items, 1.50, 42.32)
-  end
-end
-
-class PlainItemsReceiptTest < ReceiptTest
-  def test_receipt_items
-    expected = <<~HERE
-      2 book: 24.98
-      1 music CD: 16.49
-      1 chocolate bar: 0.85
-    HERE
-
-    actual = Receipt.new(@order).items
-
-    assert_equal expected, actual
+    def setup
+      @items = [
+        {quantity: 2, title: "book", subtotal: 24.98},
+        {quantity: 1, title: "music CD", subtotal: 16.49},
+        {quantity: 1, title: "chocolate bar", subtotal: 0.85},
+      ]
+      @order = Order.new(@items, 1.50, 42.32)
+    end
   end
 
-  def test_receipt_total_tax
-    expected = <<~HERE
-      Sales Taxes: 1.50
-    HERE
+  class PlainItemsReceiptTest < ReceiptTest
+    def test_receipt_items
+      expected = <<~HERE
+        2 book: 24.98
+        1 music CD: 16.49
+        1 chocolate bar: 0.85
+      HERE
 
-    actual = Receipt.new(@order).total_tax
+      actual = Receipt.new(@order).items
 
-    assert_equal expected, actual
+      assert_equal expected, actual
+    end
+
+    def test_receipt_total_tax
+      expected = <<~HERE
+        Sales Taxes: 1.50
+      HERE
+
+      actual = Receipt.new(@order).total_tax
+
+      assert_equal expected, actual
+    end
+
+    def test_receipt_total_cost
+      expected = <<~HERE
+        Total: 42.32
+      HERE
+
+      actual = Receipt.new(@order).total_cost
+
+      assert_equal expected, actual
+    end
+
+    def test_receipt_output
+      expected = <<~HERE
+        2 book: 24.98
+        1 music CD: 16.49
+        1 chocolate bar: 0.85
+        Sales Taxes: 1.50
+        Total: 42.32
+      HERE
+
+      actual = Receipt.new(@order).to_s
+
+      assert_equal expected, actual
+    end
   end
 
-  def test_receipt_total_cost
-    expected = <<~HERE
-      Total: 42.32
-    HERE
-
-    actual = Receipt.new(@order).total_cost
-
-    assert_equal expected, actual
+  class ImportedItemsReceiptTest < ReceiptTest
+    def test_foo
+      assert_equal 1,1
+    end
   end
-
-  def test_receipt_output
-    expected = <<~HERE
-      2 book: 24.98
-      1 music CD: 16.49
-      1 chocolate bar: 0.85
-      Sales Taxes: 1.50
-      Total: 42.32
-    HERE
-
-    actual = Receipt.new(@order).to_s
-
-    assert_equal expected, actual
-  end
-end
-
-class ImportedItemsReceiptTest < ReceiptTest
-  def test_foo
-    assert_equal 1,1
-  end
-end
 end
