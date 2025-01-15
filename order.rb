5d014b28908251592859c9a0119fad211f9d6d42
Brandon Toone <780144+btoone@users.noreply.github.com>
2025-01-14 23:51:43 -0700

    Calculate totals

--- order.rb
+++ order.rb
@@ -11,6 +11,9 @@ module ShoppingBasket
 
     def build_items(items)
       items.each do |item|
+        item[:subtotal] = 0.0
+        item[:tax] = 0.0
+
         item[:subtotal] = subtotal(item).round(2)
       end
     end
@@ -18,16 +21,36 @@ module ShoppingBasket
     def subtotal(item)
       subtotal = (item[:price] * item[:quantity])
 
-      # sales_tax = item[:price] * SALES_TAX_RATE
       sales_tax = round_up_to_nearest_0_05(item[:price] * SALES_TAX_RATE)
-      subtotal += sales_tax unless item[:exempt]
 
-      # import_duty = item[:price] * IMPORT_DUTY_RATE
+      unless item[:exempt]
+        subtotal += sales_tax
+        item[:tax] += sales_tax
+      end
+
       import_duty = round_up_to_nearest_0_05(item[:price] * IMPORT_DUTY_RATE)
-      subtotal += import_duty if item[:imported]
+
+      if item[:imported]
+        subtotal += import_duty
+        item[:tax] += import_duty
+      end
 
       subtotal.round(2)
     end
+
+    def total_tax
+      @items.reduce(0) do |sum, item|
+        sum + item[:tax]
+      end
+    end
+
+    def total_cost
+      @items.reduce(0) do |sum, item|
+        sum + item[:subtotal]
+      end
+    end
+
+    private
 
     def round_up_to_nearest_0_05(tax_amount)
       (tax_amount * 20).ceil / 20.0
--- receipt_test.rb
+++ receipt_test.rb
@@ -2,19 +2,20 @@ require_relative './test_helper'
 
 module ShoppingBasket
   class ReceiptTest < Minitest::Test
-    Order = Struct.new(:items, :total_tax, :total_cost)
+    # Order = Struct.new(:items, :total_tax, :total_cost)
+  end
 
+  class PlainItemsReceiptTest < ReceiptTest
     def setup
       @items = [
         {quantity: 2, title: "book", price: 12.49, exempt: true, subtotal: 24.98},
         {quantity: 1, title: "music CD", price: 14.99, exempt: false, subtotal: 16.49},
         {quantity: 1, title: "chocolate bar", price: 0.85, exempt: true, subtotal: 0.85},
       ]
-      @order = Order.new(@items, 1.50, 42.32)
-    end
-  end
 
-  class PlainItemsReceiptTest < ReceiptTest
+      @order = Order.new(@items)
+    end
+
     def test_receipt_items
       expected = <<~HERE
         2 book: 24.98
@@ -63,8 +64,26 @@ module ShoppingBasket
   end
 
   class ImportedItemsReceiptTest < ReceiptTest
-    def test_foo
-      assert_equal 1,1
+    def setup
+      @items = [
+        {quantity: 1, title: "imported box of chocolates", price: 10.00, exempt: true, imported: true},
+        {quantity: 1, title: "imported bottle of perfume", price: 47.50, exempt: false, imported: true},
+      ]
+
+      @order = Order.new(@items)
+    end
+
+    def test_receipt_output
+      expected = <<~HERE
+        1 imported box of chocolates: 10.50
+        1 imported bottle of perfume: 54.65
+        Sales Taxes: 7.65
+        Total: 65.15
+      HERE
+
+      actual = Receipt.new(@order).to_s
+
+      assert_equal expected, actual
     end
   end
 end
