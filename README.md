7bc2c3b7987efff24eb83a844656ee3a5390c5d8
Brandon Toone <780144+btoone@users.noreply.github.com>
2025-01-15 00:30:03 -0700

    Update main with examples

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Use rake to run all tests
 ```shell
 rake test
 ```
+
+## Run Main
+
+Run `main.rb` to run the program
+
+```shell
+ruby main.rb
+```
--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require_relative './receipt'
+require_relative './order'
 
-Order = Struct.new(:items, :total_tax, :total_cost)
+# Order = Struct.new(:items, :total_tax, :total_cost)
 
 items = [
   {quantity: 2, title: "book", price: 12.49, exempt: true},
@@ -8,7 +9,19 @@ items = [
   {quantity: 1, title: "chocolate bar", price: 0.85, exempt: true},
 ]
 
-order = Order.new(items, 1.50, 42.32)
+order = ShoppingBasket::Order.new(items)
+
+receipt = ShoppingBasket::Receipt.new(order)
+puts receipt
+
+puts "\n==============\n\n"
+
+items = [
+  {quantity: 1, title: "imported box of chocolates", price: 10.00, exempt: true, imported: true},
+  {quantity: 1, title: "imported bottle of perfume", price: 47.50, exempt: false, imported: true},
+]
+
+order = ShoppingBasket::Order.new(items)
 
 receipt = ShoppingBasket::Receipt.new(order)
 puts receipt
