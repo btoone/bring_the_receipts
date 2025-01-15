33f3197a5b02051f8820a860bf295b83318bdb94
Brandon Toone <780144+btoone@users.noreply.github.com>
2025-01-14 19:44:44 -0700

    Add main to run

---
+++ main.rb
@@ -0,0 +1,14 @@
+require_relative './receipt'
+
+Order = Struct.new(:items, :total_tax, :total_cost)
+
+items = [
+  {quantity: 2, title: "book", subtotal: 24.98},
+  {quantity: 1, title: "music CD", subtotal: 16.49},
+  {quantity: 1, title: "chocolate bar", subtotal: 0.85},
+]
+
+order = Order.new(items, 1.50, 42.32)
+
+receipt = Receipt.new(order)
+puts receipt
